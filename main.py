f0da92aa96f209a10205bd4250940a8520abab1a
Yohan Nugera <yohannugera@gmail.com>
2023-03-18 22:22:06 +0800

    Incremental Changes - 18/03/2023
    Some changes were made to the file structure

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import sys
+
 indentation_length = ' '
 
 def tab_level(line):
     return(len(line)-len(line.lstrip(indentation_length)))
-
 def indent_level(lines):
     line_levels = []
 
@@ -13,7 +14,6 @@ def indent_level(lines):
         line_levels.append(temp)
 
     return line_levels
-
 def dict_insert_or_append(adict,key,val):
     if key in adict:
         if type(adict[key]) != list:
@@ -21,7 +21,6 @@ def dict_insert_or_append(adict,key,val):
         adict[key].append(val)
     else:
         adict[key] = val
-
 def ttree_to_json(ttree,level=0):
     result = {}
     for i in range(0,len(ttree)):
@@ -47,9 +46,36 @@ def ttree_to_json(ttree,level=0):
             dict_insert_or_append(result,cn['line'],0)
             return result
     return result
+def parse_asa_configuration(input_raw, input_parse):
+    # Set up lists and dictionaries for return purposes
+    names = []
+    objects = {}
+    object_groups = {}
+    # Read each line of the config, looking for configuratio components that we care about
+    for line in input_raw:
+        # Identify all staticallly configured name/IPAddress translations
+        if re.match(
+                "^name (([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).*",
+                line):
+            names.append(line)
 
+        # Identify and collect configurations for all configured objects
+        if 'object network' in line:
+            obj = input_parse.find_children_w_parents(line, '^(?! nat ?.*)')
+            obj_name = (line.split()).pop(2)
+            if not obj_name in objects and obj:
+                objects[obj_name] = (obj)
+
+        # Identify and collect configurations for all configured object groups
+        if 'object-group network' in line:
+            obj_group = input_parse.find_children_w_parents(line, '.*')
+            obj_group_name = (line.split()).pop(2)
+            if not obj_group_name in object_groups and obj_group:
+                object_groups[obj_group_name] = (obj_group)
+
+    return (names, objects, object_groups)
 def main():
-    user_source_file = "running-config.cfg"
+    user_source_file = "src/running-config.cfg"
 
     # Open the source configuration file for reading and import/parse it.
     x = open(user_source_file,'r')
@@ -58,8 +84,6 @@ def main():
 
     config_level = indent_level(config_raw)
     config_tree = ttree_to_json(config_level)
-
-    
-
+    print(config_tree)
 if __name__ == '__main__':
   main()
