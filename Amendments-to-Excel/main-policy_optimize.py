25a6a32318f0e938ace65367a64ed1070b5e63e2
Yohan Nugera <yohan.s.nugera@exxonmobil.com>
2023-04-11 10:57:39 +0800

    Incremental Update from office - 2023_04_11

--- Amendments-to-Excel/main-policy_optimize.py
+++ Amendments-to-Excel/main-policy_optimize.py
@@ -1,1 +1,21 @@
 # This is where I'll configure rule-consolidation and route-checkup
+import pandas as pd
+import typer
+
+def main(config_file: str):
+    ## Reading Excel Configuration
+    file = pd.ExcelFile(config_file)
+    
+    # Read Address Groups from Excel
+    policies_file = pd.read_excel(file,'ACLs')
+    
+    new_policies_file = pd.DataFrame(policies_file.T.pop(0))
+    policies_file.drop(index=policies_file.index[0],axis=0,inplace=True)
+    
+    while len(policies_file)>0:
+        new_policies_file.append(policies_file.T.pop(0))
+        policies_file.drop(index=policies_file.index[0],axis=0,inplace=True)
+        
+
+if __name__=="__main__":
+    typer.run(main)
